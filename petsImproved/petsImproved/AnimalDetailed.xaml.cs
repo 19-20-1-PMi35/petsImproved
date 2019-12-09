0108f51b352a94f5f2df4f61c3ff18911d7a9e3d
NazarKavka <nazarkavka@users.noreply.github.com>
2019-12-09 12:22:04 +0200

    Adds alignment to name on AnimalDetailed screen and adds closing or hidding windows when other windows id open.

--- petsImproved/petsImproved/AnimalDetailed.xaml.cs
+++ petsImproved/petsImproved/AnimalDetailed.xaml.cs
@@ -30,7 +30,9 @@ namespace petsImproved
         {
             OrdersForms orderForm = new OrdersForms();
             orderForm.passId(id);
+            this.Hide();
             orderForm.ShowDialog();
+            this.Show();
         }
         public void init(int elementId)
         {
@@ -43,6 +45,13 @@ namespace petsImproved
             Age.Text = "Age: " + petInfo.age.ToString();
             Description.Text = petInfo.description;
 
+        }
+
+        private void Main_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            MainWindow main = new MainWindow();
+            main.ShowDialog();
         }
     }
 }
