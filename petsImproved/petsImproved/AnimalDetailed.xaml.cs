d9c1f6fc00953777a12f6f8d6ed3fffb3ad3b42c
Peremoha <dvtytrhenbqrjvg@gmail.com>
2019-12-09 12:06:18 +0200

    add convertation from byte[] to Image on Window

--- petsImproved/petsImproved/AnimalDetailed.xaml.cs
+++ petsImproved/petsImproved/AnimalDetailed.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -37,6 +38,20 @@ namespace petsImproved
             Sex.Text = "Sex: " + petInfo.sex;
             Age.Text = "Age: " + petInfo.age.ToString();
             Description.Text = petInfo.description;
+            File.WriteAllBytes("image.jpg", petInfo.image);
+
+            MemoryStream memorystream = new MemoryStream();
+            memorystream.Write(petInfo.image, 0, (int)petInfo.image.Length);
+            
+            BitmapImage imgsource = new BitmapImage();
+
+            memorystream.Seek(0, SeekOrigin.Begin);
+
+            imgsource.BeginInit();
+            imgsource.StreamSource = memorystream;
+            imgsource.EndInit();
+            NImage.Source = imgsource;
+
 
         }
     }
